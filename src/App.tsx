6bc3788d1dc86f7077405e51952fb8a1a59443a7
Cody Moore <codymoore240@gmail.com>
2020-07-11 12:34:40 -0400

    Got the boring stuff out of the way

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'assets/styles/base.module.scss'
+import Logo from 'components/Logo'
+
+function App() {
+  return (
+    <div>
+      <Logo
+        text={`codymoore.io`}
+      ></Logo>
+      Here it is
+      </div>
+  )
+}
+
+export default App
+
